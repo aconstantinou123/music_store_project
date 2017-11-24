65d854a805d5ecc7a8245facc69f1886ff87591d
aconstantinou123 <a_constantinou@hotmail.co.uk>
2017-11-24 14:31:33 +0000

    album model create with delete_all and save

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,6 @@
 require('pry-byebug')
 require_relative('../models/artist.rb')
+require_relative('../models/album.rb')
 
 Artist.delete_all()
 
@@ -15,6 +16,9 @@ artist5 = Artist.new('name' => 'Burzum')
 artist5.save()
 
 Artist.list_all()
+
+album1 = Album.new('title' => 'In the Nightside Eclipse', 'artist_id' => artist1.id, 'quantity' => 7)
+album1.save()
 
 binding.pry
 nil
---
+++ models/album.rb
@@ -0,0 +1,31 @@
+require_relative('../db/sql_runner.rb')
+
+class Album
+
+  attr_reader :id, :title, :artist_id, :quantity
+
+  def initialize(options)
+    @id = options['id'].to_i if options['id']
+    @title = options['title']
+    @artist_id = options['artist_id'].to_i
+    @quantity = options['quantity']
+  end
+
+  def self.delete_all()
+    sql = "DELETE FROM albums"
+    SqlRunner.run(sql)
+  end
+
+  def save()
+    sql = 'INSERT INTO albums (
+    title,
+    artist_id,
+    quantity
+    ) VALUES ( $1, $2, $3 )
+    RETURNING *'
+    values = [@title, @artist_id, @quantity]
+    @id = SqlRunner.run(sql, values)[0]['id'].to_i
+  end
+
+
+end
