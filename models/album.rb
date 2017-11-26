f1853abd76b62f76b141445e046147815ab0d231
aconstantinou123 <a_constantinou@hotmail.co.uk>
2017-11-26 16:20:52 +0000

    buy/sell/markup columns added to html

--- models/album.rb
+++ models/album.rb
@@ -2,7 +2,7 @@ require_relative('../db/sql_runner.rb')
 
 class Album
 
-  attr_reader :id, :title, :artist_id, :quantity
+  attr_reader :id, :title, :artist_id, :buy_price, :sell_price, :quantity
 
   def initialize(options)
     @id = options['id'].to_i
